76617f6a1dfed89813c4ad62a24766b45532c4df
Miguel A. Vico <mvicomoya@nvidia.com>
2017-12-12 21:00:07 +0100

    nouveau: Allocate nouveau device from DRM fd
    Add nouveau_driver_t and nouveau_device_t structures
    and the required logic to allocate a nouveau
    allocator device.
    
    Signed-off-by: Miguel A Vico Moya <mvicomoya@nvidia.com>

--- src/drivers/nouveau/nouveau_allocator_driver.c
+++ src/drivers/nouveau/nouveau_allocator_driver.c
@@ -22,13 +22,50 @@
 
 #include <allocator/driver.h>
 
+#include <sys/stat.h>
+#include <unistd.h>
+#include <xf86drm.h>
+#include <nouveau.h>
+#include <nouveau_drm.h>
+#include <nvif/class.h>
+#include <nvif/cl0080.h>
+
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/* Nouveau allocator driver and device definitions */
+typedef struct {
+    driver_t *pbase;
+} nouveau_driver_t;
+
+typedef struct {
+    device_t base;
+
+    struct nouveau_drm *drm;
+    struct nouveau_device *dev;
+
+    struct {
+        uint64_t address_alignment;
+        uint32_t pitch_alignment;
+        uint32_t max_pitch;
+        uint32_t max_dimensions;
+    } properties;
+} nouveau_device_t;
 
 static int
 nouveau_is_fd_supported(driver_t *driver, int fd)
 {
-    /* TODO */
-    return 0;
+    /* XXX: This only checks whether the given fd is a DRM device. Note that
+     *      this doesn't necessarily means it's a nouveau device */
+    switch (drmGetNodeTypeFromFd(fd)) {
+    case DRM_NODE_PRIMARY:
+    case DRM_NODE_CONTROL:
+    case DRM_NODE_RENDER:
+        return 1;
+    default:
+        return 0;
+    }
 }
 
 static int
@@ -80,45 +117,102 @@ static int nouveau_device_get_allocation_fd(device_t *dev,
 
 static void nouveau_device_destroy(device_t *device)
 {
-    if (device) {
-        /* TODO: Private device destruction */
+    nouveau_device_t *nouveau_device = device->device_private;
+
+    if (nouveau_device) {
+        nouveau_device_del(&nouveau_device->dev);
+        nouveau_drm_del(&nouveau_device->drm);
         free(device);
     }
 }
 
 static device_t *nouveau_device_create_from_fd(driver_t *driver, int fd)
 {
-    device_t *device;
+    nouveau_device_t *nouveau_device;
+    int ret;
 
-    /* TODO: Sanity checks on fd */
-
-    device = (device_t *)calloc(1, sizeof(device_t));
-    if (!device) {
+    if (!driver->is_fd_supported(driver, fd)) {
         return NULL;
     }
 
-    /* TODO: Private device creation */
-    device->device_private = NULL;
+    nouveau_device = (nouveau_device_t *)calloc(1, sizeof(nouveau_device_t));
+    if (!nouveau_device) {
+        return NULL;
+    }
 
-    device->driver = driver;
+    ret = nouveau_drm_new(fd, &nouveau_device->drm);
+    if (ret) {
+        goto fail;
+    }
 
-    device->destroy = &nouveau_device_destroy;
-    device->get_capabilities = &nouveau_device_get_capabilities;
-    device->get_assertion_hints = &nouveau_device_get_assertion_hints;
-    device->create_allocation = &nouveau_device_create_allocation;
-    device->destroy_allocation = &nouveau_device_destroy_allocation;
-    device->get_allocation_fd = &nouveau_device_get_allocation_fd;
+    ret = nouveau_device_new(&nouveau_device->drm->client, NV_DEVICE,
+                             &(struct nv_device_v0) {
+                                 .device = ~0LL,
+                             }, sizeof(struct nv_device_v0),
+                             &nouveau_device->dev);
+    if (ret) {
+        goto fail;
+    }
 
-    return device;
+    nouveau_device->base.device_private = nouveau_device;
+    nouveau_device->base.driver = driver;
+    nouveau_device->base.destroy = &nouveau_device_destroy;
+    nouveau_device->base.get_capabilities = &nouveau_device_get_capabilities;
+    nouveau_device->base.get_assertion_hints = &nouveau_device_get_assertion_hints;
+    nouveau_device->base.create_allocation = &nouveau_device_create_allocation;
+    nouveau_device->base.destroy_allocation = &nouveau_device_destroy_allocation;
+    nouveau_device->base.get_allocation_fd = &nouveau_device_get_allocation_fd;
+
+    switch (nouveau_device->dev->chipset & ~0xf) {
+    /* Fermi */
+    case 0xc0:
+    case 0xd0:
+    /* Kepler */
+    case 0xe0:
+    case 0xf0:
+    case 0x100:
+    /* Maxwell */
+    case 0x110:
+    case 0x120:
+    /* Pascal */
+    case 0x130:
+        nouveau_device->properties.address_alignment = 4096;
+        nouveau_device->properties.pitch_alignment = 128;
+        nouveau_device->properties.max_pitch = INT_MAX;
+        nouveau_device->properties.max_dimensions = 16384;
+        break;
+    default:
+        nouveau_device->properties.address_alignment = 4096;
+        nouveau_device->properties.pitch_alignment = 64;
+        nouveau_device->properties.max_pitch = INT_MAX;
+        nouveau_device->properties.max_dimensions = 16384;
+        break;
+    }
+
+    return &nouveau_device->base;
+
+fail:
+    nouveau_device_del(&nouveau_device->dev);
+    nouveau_drm_del(&nouveau_device->drm);
+    free(nouveau_device);
+    return NULL;
 }
 
 static void nouveau_driver_destroy(driver_t *driver)
 {
-    /* TODO */
+    nouveau_driver_t *nouveau_driver =
+        (nouveau_driver_t *)driver->driver_private;
+
+    if (nouveau_driver) {
+        free(nouveau_driver);
+        driver->driver_private = NULL;
+    }
 }
 
 int allocator_driver_init(driver_t *driver)
 {
+    nouveau_driver_t *nouveau_driver;
+
     // Workaround for compiler warning that no unsigned value is < 0
     const unsigned int our_header_version = DRIVER_INTERFACE_VERSION;
 
@@ -126,8 +220,14 @@ int allocator_driver_init(driver_t *driver)
         return -1;
     }
 
-     /* TODO: Private driver creation */
-    driver->driver_private = NULL;
+    nouveau_driver = (nouveau_driver_t *)calloc(1, sizeof(nouveau_driver_t));
+    if (!nouveau_driver) {
+        return -1;
+    }
+
+    nouveau_driver->pbase = driver;
+
+    driver->driver_private = (void *)nouveau_driver;
 
     driver->driver_interface_version = DRIVER_INTERFACE_VERSION;
 
